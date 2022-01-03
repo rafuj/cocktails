ca2ef1ddb63396d1e7ac52240a24a8a85747501c
rafuj <raihanrafuj0078@gmail.com>
2022-01-03 16:39:22 +0600

    Get Cocktails Fetching api cocktail, create cocktail single page and Create Search Option, Update Context API

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { useGlobalContext } from "../context";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
 
 const CocktailList = () => {
+  const { loading, cocktails } = useGlobalContext();
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">No Cocktails Available Your Search</h2>
+    );
+  }
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
+    <section className="section">
+      <h2 className="section-title">Cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
   );
 };
 
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = useRef("");
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">Search Your Favorite Cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            // onChange={(e) => setSearchTerm(e.target.value)}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
